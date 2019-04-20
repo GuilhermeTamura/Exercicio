00721351455fdf2bb4f15c7fff990991a14469cf
GuilhermeTamura <gui14sonho@gmail.com>
2019-04-20 10:09:22 -0300

    Aula de LPO

---
+++ src/exercicio/Execucao.java
@@ -0,0 +1,15 @@
+package exercicio;
+
+public class Execucao {
+
+	public static void main(String[] args) {
+		Pessoa sg = new Pessoa();
+		sg.Andando();
+		sg.Respirando();
+		 
+		
+		
+
+	}
+
+}
---
+++ src/exercicio/Pessoa.java
@@ -0,0 +1,15 @@
+package exercicio;
+
+public class Pessoa {
+	
+	public void Andando() {
+		System.out.println("Andando");
+	}
+	protected void Respirando() {
+		System.out.println("Respirando");
+	}
+	private void Dormindo() {
+		System.out.println("Dormindo");
+	}
+
+}
